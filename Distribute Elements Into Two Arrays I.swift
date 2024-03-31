1643cf0cf392f5b0daa586b451959f8d19582cbe
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2024-03-31 13:31:05 +0530

    Create Distribute Elements Into Two Arrays I.swift
    Day 9: Distribute Elements Into Two Arrays I

---
+++ Distribute Elements Into Two Arrays I.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func resultArray(_ nums: [Int]) -> [Int] {
+        var array1 = [Int]()
+        var array2 = [Int]()
+
+        array1.append(nums[0])
+        array2.append(nums[1])
+        for i in 2..<nums.count {
+            if array1[array1.count-1] > array2[array2.count-1] {
+                array1.append(nums[i])
+            } else {
+                array2.append(nums[i])
+            }
+        }
+
+        return array1 + array2
+    }
+}
