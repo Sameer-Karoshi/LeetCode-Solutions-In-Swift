a841ec673edf2f3ccd4062e750432f50bfc76607
Sameer  Karoshi <65184810+Sameer-Karoshi@users.noreply.github.com>
2025-07-22 20:47:18 +0530

    Day 41: Longest Substring Without Repeating Characters.swift

--- Longest Substring Without Repeating Characters.swift
+++ Longest Substring Without Repeating Characters.swift
@@ -21,3 +21,28 @@ class Solution {
         return result
     }
 }
+
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var map: [Character: String.Index] = [:]
+        var result = 0
+        var left = s.startIndex
+        var right = s.startIndex
+
+        while right < s.endIndex {
+            let char = s[right]
+
+            if let prevIndex = map[char], prevIndex >= left {
+                left = s.index(after: prevIndex)
+            }
+
+            map[char] = right
+            let length = s.distance(from: left, to: right) + 1
+            result = max(result, length)
+
+            right = s.index(after: right)
+        }
+
+        return result
+    }
+}
